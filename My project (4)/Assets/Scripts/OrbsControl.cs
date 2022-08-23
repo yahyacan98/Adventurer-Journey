c0536a8ddd235a1171977819830dd7d69801cc81
yahyacan98 <sahanyahya@hotmail.com>
2022-08-23 13:29:47 +0300

    .
    .

--- My project (4)/Assets/Scripts/OrbsControl.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
-public class OrbsControl : MonoBehaviour
-{
-    [SerializeField] public int DestroyedOrbs = 0;
-}
